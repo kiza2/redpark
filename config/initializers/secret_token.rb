2ed2c41a38418278c956c929674258f2296ac820
Alex Yu <alexyu2@gmail.com>
2015-06-27 15:03:10 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redpark::Application.config.secret_token = '5f7b9b81eb5a58c7e046bec5bb42ea672a97db64d46a1e0529a20110da3538ba7d57913ecd745d7b15986929310d871ab9841060682a49012a97839ef12de71f'
